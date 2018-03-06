085853a28bf2d0c5e6d7eaad72c968660fa1a6ad
Colin Fendrick <colin.fendrick@gmail.com>
2018-03-06 12:41:02 -0500

    Using a django model to search

--- links/schema.py
+++ links/schema.py
@@ -4,6 +4,7 @@ from graphene_django import DjangoObjectType
 from links.models import Link, Vote
 from users.schema import UserType
 from graphql import GraphQLError
+from django.db.models import Q
 
 
 class LinkType(DjangoObjectType):
@@ -16,10 +17,20 @@ class VoteType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    links = graphene.List(LinkType)
+    # Add the search parameter inside our links field
+    links = graphene.List(LinkType, search=graphene.String())
     votes = graphene.List(VoteType)
 
-    def resolve_links(self, info, **kwargs):
+    # Change the resolver
+    def resolve_links(self, info, search=None, **kwargs):
+        # The value sent with the search parameter will be on the args variable
+        if search:
+            filter = (
+                Q(url__icontains=search) | 
+                Q(description__icontains=search)
+            )
+            return Link.objects.filter(filter)
+
         return Link.objects.all()
 
     def resolve_votes(self, info, **kwargs):
