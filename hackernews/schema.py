7db7ccd43ab1e2a177f2791053a6d1ff17a1a723
Colin Fendrick <colin.fendrick@gmail.com>
2018-03-06 09:36:14 -0500

    Updated with mutation

--- hackernews/schema.py
+++ hackernews/schema.py
@@ -6,5 +6,11 @@ import links.schema
 class Query(links.schema.Query, graphene.ObjectType):
     pass
 
+class Mutation(links.schema.Mutation, graphene.ObjectType):
+    pass
 
-schema = graphene.Schema(query=Query)
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
+
+
+
--- links/schema.py
+++ links/schema.py
@@ -14,3 +14,30 @@ class Query(graphene.ObjectType):
 
     def resolve_links(self, info, **kwargs):
         return Link.objects.all()
+
+# defines a mutation class
+class CreateLink(graphene.Mutation):
+    id = graphene.Int()
+    url = graphene.String()
+    description = graphene.String()
+
+    # defines data that can be sent to server, ie url and description
+    class Arguments:
+        url = graphene.String()
+        description = graphene.String()
+
+    # Mutation method: creates a link on database using url and description, 
+    # and server returns CreateLink class with data
+    def mutate(self, info, url, description):
+        link = Link(url=url, description=description)
+        link.save()
+
+        return CreateLink(
+            id=link.id,
+            url=link.url,
+            description=link.description
+        )
+
+# Creates a mutation class with a field to be resolved, pointing to previously defined mutation
+class Mutation(graphene.ObjectType):
+    create_link = CreateLink.Field()
